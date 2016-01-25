d9fdd5cc041e8d50991317f989cacc5fa6e0570a
Gitle Mikkelsen <gitlem@gmail.com>
2016-01-25 17:44:52 +0100

    added point type and started point output function

--- arduino_due/lasdac_arduino/lasdac_arduino/src/main.c
+++ arduino_due/lasdac_arduino/lasdac_arduino/src/main.c
@@ -6,15 +6,27 @@ Lasdac main prosjekt (for Arduino Due)
 #include "asf.h"
 //#include <setup.c>
 
-void setup_spi(void);
-void setup_dac(void);
-
 //states
 #define STATE_IDLE 0
-
 uint8_t state = STATE_IDLE;
 
-//systick timer ISR: freq controlled by main.timerFreq
+//structs
+typedef struct
+{
+	uint16_t x;
+	uint16_t y;
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+	uint8_t a;
+} point;
+
+//functions
+void setup_spi(void);
+void setup_dac(void);
+void output_point(point);
+
+//systick timer ISR
 void SysTick_Handler(void)
 {
 	
@@ -30,11 +42,22 @@ int main (void)
 	
 }
 
+void output_point(point point_arg)
+{
+	if ((dacc_get_interrupt_status(DACC) & DACC_ISR_TXRDY) == DACC_ISR_TXRDY) //if DAC ready
+	{
+		dacc_set_channel_selection(DACC, 0);
+		dacc_write_conversion_data(DACC, (uint32_t)point_arg.x);
+		dacc_set_channel_selection(DACC, 1);
+		dacc_write_conversion_data(DACC, (uint32_t)point_arg.y);
+	}
+	//TODO: SPI for RGBA (GPIO for A?)
+}
 
 void setup_spi(void)
 {
 	//setup SPI for DAC084S085
-	//IKKE FERDIG
+	//TODO
 	
 }
 
